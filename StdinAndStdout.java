3fa107415a665087fbf69d2932bdb575fdef3596
omsah <huncyoms@gmail.com>
2023-04-27 09:21:54 -0400

    commit

---
+++ StdinAndStdout.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class StdinAndStdout {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int int1 = sc.nextInt();
+        int int2 = sc.nextInt();
+        int int3 = sc.nextInt();
+        
+        System.out.println(int1);
+        System.out.println(int2);
+        System.out.println(int3);
+    }
+}
