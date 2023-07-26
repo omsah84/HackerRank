4983e2154d6b2df0ef1be6d3f0d2b4fdbac9bb99
OM SAH <huncyoms@gmail.com>
2023-07-26 07:47:53 -0400

    Hackerrank

---
+++ CPP_Problem_Solving_Series/Input-and-output.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    // string s;
+    // int n;
+    
+    // cin>> s >> n;
+    
+    // cout<< s<<" "<< n<< endl;
+    
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    int sum = 0;
+    sum = a+b+c;
+    cout<<sum;
+    
+    return 0;
+}
