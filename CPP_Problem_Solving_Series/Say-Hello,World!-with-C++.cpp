42ef1ff3e5957d9f17de40ff17135cbe46ed699e
OM SAH <huncyoms@gmail.com>
2023-07-26 07:46:03 -0400

    hakerrank

---
+++ CPP_Problem_Solving_Series/Say-Hello,World!-with-C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // printf("Hello, World!");
+    
+    char PrintHello[] = "Hello, World!";
+    cout<<PrintHello;
+    return 0;
+}
